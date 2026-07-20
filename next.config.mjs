7d78d52fe2684b9267f814f824ab856351318327
Sachin <sachinsharma8005@gmail.com>
2026-07-21 00:37:45 +0530

    csp removed

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,6 @@ const nextConfig = {
           { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-          { key: 'Content-Security-Policy', value: "default-src 'self';" } 
         ],
       },
     ]
